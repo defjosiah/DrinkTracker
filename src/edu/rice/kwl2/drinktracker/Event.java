9474821081ff0cc70c1f149ad6d5885619621530
sjl0 <seth.j.lauer@gmail.com>
2014-01-25 20:45:18 -0600

    Constructor, addDrink, calcBAC method create for Event

--- src/edu/rice/kwl2/drinktracker/Event.java
+++ src/edu/rice/kwl2/drinktracker/Event.java
@@ -1,5 +1,31 @@
 package edu.rice.kwl2.drinktracker;
+import java.util.ArrayList;
 
 public class Event {
+	
+	private Person drinker;
+	private DateTime startTime;
+	private String date;
+	private ArrayList<Drink> drinksList;
+	
+	public Event (Person drinker, String startTime, String date) {
+		this.drinker = drinker;
+		this.startTime = startTime;
+		this.date = date;
+	}
+	
+	public void addDrink (Drink newDrink) {
+		drinksList.add(newDrink);
+	}
+	
+	public double calcBAC() {
+		DateTime timeNow = DateTime.now();
+		return ((.806 * SD * 1.2) / (BW * Wt)) - (MR * DP);
+	}
+	
+	/*
+	 * Given a number of hours in the future (or negative), calculates BAC
+	 */
+	public double calcBAC(double xHours) {}
 
 }
