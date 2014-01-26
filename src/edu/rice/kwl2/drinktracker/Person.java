0b968e0783d54b27fc59af2883a38dfc16c2cd12
sjl0 <seth.j.lauer@gmail.com>
2014-01-25 20:29:27 -0600

    Getters and Setters for Person

--- src/edu/rice/kwl2/drinktracker/Person.java
+++ src/edu/rice/kwl2/drinktracker/Person.java
@@ -1,5 +1,52 @@
 package edu.rice.kwl2.drinktracker;
 
 public class Person {
+	
+	private String name;
+	private double weight;
+	private Height height;
+	private String gender;
+	
+	Person(String name, double weight, Height height, String gender) {
+		this.name = name;
+		this.weight = weight;
+		this.height = height;
+		this.gender = gender;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public Height getHeight() {
+		return height;
+	}
+
+	public void setHeight(Height height) {
+		this.height = height;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	
 
 }
