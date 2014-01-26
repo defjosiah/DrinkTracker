d33a557b7fe6ef09e99089a229ec65a9877cb089
newscannerd <elsey.andrew@gmail.com>
2014-01-25 21:04:59 -0600

    syncing everything so far

--- src/edu/rice/kwl2/drinktracker/MainActivity.java
+++ src/edu/rice/kwl2/drinktracker/MainActivity.java
@@ -2,6 +2,8 @@ package edu.rice.kwl2.drinktracker;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -18,6 +20,12 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void sendBeer(View view) {
+        System.out.println("Beer added");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
     }
     
 }
